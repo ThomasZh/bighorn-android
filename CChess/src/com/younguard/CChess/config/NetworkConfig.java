e06afb7b418f2eddfbb55a5cfbdd6ebe09b24731
chengxiaocai <912627598@qq.com>
2015-03-10 20:56:07 +0800

    chess  first commit

---
+++ CChess/src/com/younguard/CChess/config/NetworkConfig.java
@@ -0,0 +1,11 @@
+package com.younguard.CChess.config;
+
+/**
+ * Created by liwenzhi on 14-10-13.
+ */
+public class NetworkConfig {
+
+    public static int RESPONSE_OK = 100;
+    public static int RESPONSE_FAILURE = 200;
+
+}
---
+++ CChess/src/com/younguard/CChess/param/GatewayParams.java
@@ -0,0 +1,43 @@
+package com.younguard.CChess.param;
+
+/**
+ * Created by liwenzhi on 14-9-24.
+ */
+public class GatewayParams {
+    /**
+     * client connect use
+     */
+    private String appServerIp;
+    /**
+     * client connect use
+     */
+    private int appServerPort;
+    /**
+     * client login use
+     */
+    private String appToken;
+
+    public String getAppServerIp() {
+        return appServerIp;
+    }
+
+    public void setAppServerIp(String appServerIp) {
+        this.appServerIp = appServerIp;
+    }
+
+    public int getAppServerPort() {
+        return appServerPort;
+    }
+
+    public void setAppServerPort(int appServerPort) {
+        this.appServerPort = appServerPort;
+    }
+
+    public String getAppToken() {
+        return appToken;
+    }
+
+    public void setAppToken(String appToken) {
+        this.appToken = appToken;
+    }
+}
